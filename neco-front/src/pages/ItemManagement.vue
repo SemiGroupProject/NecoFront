18ef050d7bd0f2b34fe18690f7b5f24b033ec348
Taeyoon <tyyu@lsis.com>
2021-08-29 00:41:16 +0900

    [Feat] 상품 관리 페이지

--- neco-front/src/pages/ItemManagement.vue
+++ neco-front/src/pages/ItemManagement.vue
@@ -1,11 +1,47 @@
 <template>
-  <div>
-    <h1>Item Management</h1>
-  </div>
+  <v-main style="margin: 0 12.5%">
+    <v-container fluid>
+      <h2>상품관리</h2>
+      <v-data-table
+        :headers="headers"
+        :items="productList"
+        :items-per-page="5"
+        @click:row="clickProduct"
+        style="cursor: pointer"
+        class="ma-5"
+      ></v-data-table>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      headers: [
+        { text: '상품 이미지', value: 'image' },
+        { text: '상품 이름', value: 'name' },
+        { text: '상품 가격', value: 'price' },
+        { text: '등록시간', value: 'time' }
+      ],
+      productList: []
+    };
+  },
+  methods: {
+    clickProduct() {
+      console.log('click product');
+      // this.$router.push({
+      //   name: 'notice-item',
+      //   params: { id: data.id }
+      // });
+    }
+  }
+};
 </script>
 
-<style></style>
+<style>
+h2 {
+  margin-top: 50px;
+  color: #8f68d6;
+}
+</style>
