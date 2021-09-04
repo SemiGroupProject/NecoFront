89c8da130b720dbd36fff24b3eae5fe600fa9376
Taeyoon <tyyu@lsis.com>
2021-09-05 02:00:04 +0900

    [Feat] 개별 아이템 페이지

---
+++ neco-front/src/cards/item/RelationItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-card flat>
+    <h4>연관 상품</h4>
+    <v-row>
+      <v-col cols="2" v-for="item in items" :key="item.name">
+        <v-img :src="require(`@/img/${item.image}`)" height="200"></v-img>
+        <p class="item-name">{{ item.name }}</p>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          name: '빅사이즈 옷',
+          image: 'test_image.jpg'
+        },
+        {
+          name: '덩크 로우',
+          image: 'test_image.jpg'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+h4 {
+  margin-bottom: 15px;
+}
+.item-name {
+  margin-top: 10px;
+  text-align: center;
+}
+</style>
--- neco-front/src/pages/Item.vue
+++ neco-front/src/pages/Item.vue
@@ -1,13 +1,31 @@
 <template>
   <v-main style="margin: 0 12.5%">
     <v-container fluid>
-      <h2>item</h2>
+      <image-property></image-property>
+      <relation-item></relation-item>
+      <information-text></information-text>
+      <question-input></question-input>
+      <question-reply></question-reply>
     </v-container>
   </v-main>
 </template>
 
 <script>
-export default {};
+import ImageProperty from '@/cards/item/ImageProperty.vue';
+import RelationItem from '@/cards/item/RelationItem.vue';
+import InformationText from '@/cards/item/InformationText.vue';
+import QuestionInput from '@/cards/item/QuestionInput.vue';
+import QuestionReply from '@/cards/item/QuestionReply.vue';
+
+export default {
+  components: {
+    ImageProperty,
+    RelationItem,
+    InformationText,
+    QuestionInput,
+    QuestionReply
+  }
+};
 </script>
 
 <style></style>
